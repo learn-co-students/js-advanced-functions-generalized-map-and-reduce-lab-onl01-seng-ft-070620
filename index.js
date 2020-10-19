78e1da480814d7e8be47c530c970abf338b6f8aa
Ethan <eleetyson@gmail.com>
2020-10-19 11:32:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your functions here
+// generalized map()
+function map(arr, func) {
+  let newArr = []
+  for (const element of arr) {
+    newArr.push(func(element))
+  }
+  return newArr
+}
+
+// generalized reduce()
+// unsure how to actually do this
+function reduce(arr, func, start=0) {
+  if (arr.includes(false)) return false
+  if (arr.includes(true)) return true
+
+  let accum = start
+  for (const element of arr) {
+    accum = func(element, accum)
+  }
+  return accum
+}
