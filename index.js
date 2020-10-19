dda94b1bf3ea70f7bb09a55592aff6daa072f2fe
bbbtttiii <ben.iburg@gmail.com>
2020-10-19 12:58:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+function map(arr, callback) {
+    let result = [];
+    for (let i=0;i<arr.length;i++) {
+        let e = arr[i];
+        result.push(callback(e));
+    }
+    return result;
+}
+
+function reduce(arr, callback, start) {
+    let result = (!!start) ? start : arr[0];
+    let i = (!!start) ? 0 : 1;
+    for (; i < arr.length; i++) {
+        result = callback(arr[i], result);
+    }
+    return result;
+}
