e9289cb0f5da037e3dd807600e08e7c68f0e9a76
Directrix777 <andrew.j.ribas@gmail.com>
2020-10-19 17:28:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(array, mapper)
+{
+    let newArray = []
+    for(let thing of array)
+    {
+        newArray.push(mapper(thing))
+    }
+    return newArray
+}
+
+function reduce(array, reducer, starting = array[0])
+{
+    let total = starting
+    for(let thing of array)
+    {
+        if (thing != array[0] || starting != array[0])
+        {
+            total = reducer(thing, total)
+            console.log(total)
+        }
+        
+    }
+    return total
+}
