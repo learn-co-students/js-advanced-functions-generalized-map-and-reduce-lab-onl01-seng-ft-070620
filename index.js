27071ce878dbf786665cb48429faecae203f6594
JacksonReynolds <jwreynod@gmail.com>
2020-10-19 15:03:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your functions here
+function map(arr, fxn) {
+    let newArr = []
+    for (const val of arr) {
+        newArr.push(fxn(val))
+    }
+    return newArr
+}
+
+function reduce(arr, fxn, startingPt = arr.shift()) {
+    let value = startingPt
+    for (const val of arr) {
+        value = fxn(val, value)
+    }
+    return value
+}
