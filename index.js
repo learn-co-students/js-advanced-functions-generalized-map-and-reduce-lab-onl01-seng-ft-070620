25387e82c4e06089b977b70b4caf6b98dabd9839
wickbushong <wickbushong@gmail.com>
2020-10-19 13:07:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your functions here
+function map(array, f) {
+    let ans = []
+    array.forEach(e => ans.push(f(e)))
+    return ans
+}
+
+function reduce(source, f, start) {
+    let starting = (!!start) ? start : source[0]
+    let i = (!!start) ? 0 : 1
+    for(; i < source.length; i++) {
+        starting = f(source[i], starting)
+    }
+    return starting
+}
